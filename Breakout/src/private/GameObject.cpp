4e01650c70e73d7b38f57718ab31fb4d4f3fdaf9
Hubert Aschermann <hubert@aschermann.cz>
2024-03-15 17:16:32 +0100

    build: game object

---
+++ Breakout/src/private/GameObject.cpp
@@ -0,0 +1,23 @@
+#include "GameObject.h"
+
+GameObject::GameObject() : m_position(0.0f, 0.0f), m_size(1.0f, 1.0f), m_velocity(0.0f, 0.0f),
+    m_color(1.0f), m_rotation(0.0f), m_isSolid(false), m_destroyed(false), m_sprite()
+{
+}
+
+GameObject::GameObject(glm::vec2 pos, glm::vec2 size, Texture2D sprite, glm::vec3 color,
+    glm::vec2 velocity)
+    : m_position(pos), m_size(size), m_velocity(velocity), m_color(color), m_rotation(0.0f),
+    m_isSolid(false), m_destroyed(false), m_sprite(sprite)
+{
+}
+
+void GameObject::Draw(SpriteRenderer& renderer)
+{
+    renderer.DrawSprite(m_sprite, m_position, m_size, m_rotation, m_color);
+}
+
+bool GameObject::isDestroyed() const
+{
+    return m_destroyed;
+}
---
+++ Breakout/src/public/GameObject.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+
+#include "SpriteRenderer.h"
+
+class GameObject {
+public:
+    GameObject();
+    GameObject(glm::vec2 pos, glm::vec2 size, Texture2D sprite, glm::vec3 color = glm::vec3(1.0f),
+        glm::vec2 velocity = glm::vec2(0.0f, 0.0f));
+
+    virtual void Draw(SpriteRenderer& renderer);
+
+    bool isDestroyed() const;
+
+private:
+    glm::vec2 m_position, m_size, m_velocity;
+    glm::vec3 m_color;
+    float m_rotation;
+    bool m_isSolid;
+    bool m_destroyed;
+
+    Texture2D m_sprite;
+};
